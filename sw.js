f0f3bfa9184e44aa4e44ef64315ed46601f7e3d4
norest4d-max <no_rest4d@icloud.com>
2026-05-17 03:13:13 -0700

    Add offline service worker

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = 'donut-sudoku-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.webmanifest',
+  './icons/icon.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
